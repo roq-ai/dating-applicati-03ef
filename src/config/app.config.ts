cdc7dbb77bf489e25618476112124fc4f82dafad
roq-ai <>
2023-10-25 01:47:46 +0000

    Initial commit

---
+++ src/config/app.config.ts
@@ -0,0 +1,22 @@
+interface AppConfigInterface {
+  ownerRoles: string[];
+  customerRoles: string[];
+  tenantRoles: string[];
+  tenantName: string;
+  applicationName: string;
+  addOns: string[];
+  ownerAbilities: string[];
+  customerAbilities: string[];
+  getQuoteUrl: string;
+}
+export const appConfig: AppConfigInterface = {
+  ownerRoles: ['Owner'],
+  customerRoles: [],
+  tenantRoles: ['Owner', 'Administrator'],
+  tenantName: 'Company',
+  applicationName: 'DATING APPLICATIONS',
+  addOns: ['file upload', 'chat', 'notifications', 'file'],
+  customerAbilities: [],
+  ownerAbilities: ['Manage users', 'Manage companies'],
+  getQuoteUrl: 'https://app.roq.ai/proposal/5bf8aeb4-5e8a-438a-876c-c6e3192882f7',
+};
